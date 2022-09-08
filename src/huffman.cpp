dac5513ca9a3e425205a8e227984639c378a3562
PedroLouback <pcampos952@gmail.com>
2022-09-07 23:35:36 -0300

    Min/Max repetition

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -3,9 +3,10 @@
 void ReadDocument(Lista *l)
 {
     Item aux;
+    Block *aux2;
     ifstream file;
     string line, text;
-    int repetition_count = 0, verify = 1;
+    int repetition_count = 0, verify = 1, max_repetition = 0, min_repetition = 0;
     vector<string> tokens;
     vector<string> verified_words;
 
@@ -47,24 +48,51 @@ void ReadDocument(Lista *l)
             {
                 break;
             }
-            verify = 1;                       // colocar um jeito para que coloque no vector de verified apenas uma vez
+            verify = 1;                     
             if (tokens.at(i) == tokens.at(j)) // the recurrence check will be done
             {
-                verified_words.push_back(tokens.at(i));
+                if(repetition_count == 0){
+                    verified_words.push_back(tokens.at(i));
+                }
                 repetition_count++; // if it finds a recurrence, it increases the counter by one
             }
         }
-        if (verify != 2)
+        if (verify != 2 && (i < (tokens.size() - 1))) // total size -1 because the last word was being inserted again because the verification could not be done
         {
             aux.repetition_number = (repetition_count+1);
             aux.word = tokens.at(i);
             LInsert(l, aux);
-            cout << aux.word << " - " << aux.repetition_number << endl;
             repetition_count = 0;
         }
     }
 
     LImprime(l);
+
+    // search for maximum repetition
+    aux2 = l->first->prox;
+    max_repetition = aux2->data.repetition_number;
+    while(aux2 != NULL){
+        if(aux2->data.repetition_number > max_repetition){
+            max_repetition = aux2->data.repetition_number;
+        }
+        aux2 = aux2->prox;
+    }
+
+    cout << "\n";
+    
+
+    aux2 = l->first->prox;
+    min_repetition = aux2->data.repetition_number;
+    while (aux2 != NULL)
+    {
+        if (aux2->data.repetition_number < min_repetition)
+        {
+            min_repetition = aux2->data.repetition_number;
+        }
+        aux2 = aux2->prox;
+    }
+
+    
 }
 
 string WordTreatment(string word)
